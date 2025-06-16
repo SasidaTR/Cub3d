4599812c56c5ba41d5784630e66d51d36181a982
Tommy Rischmann <trischma@f4r7s12.clusters.42paris.fr>
2025-06-16 16:08:15 +0200

    Push automatique :)

--- src/parsing/map.c
+++ src/parsing/map.c
@@ -68,3 +68,35 @@ void	display_map(t_data *d)
 	mlx_put_image_to_window(d->mlx, d->win, img, 0, 0);
 	mlx_destroy_image(d->mlx, img);
 }
+
+/*
+#include "../../include/cub3d.h"
+
+int	check_wall_collision(t_data *data, double new_x, double new_y)
+{
+	int	map_x;
+	int	map_y;
+	int	map_height;
+
+	if (!data || !data->map.map)
+		return (1);
+		
+	map_height = 0;
+	while (data->map.map[map_height])
+		map_height++;
+		
+	map_x = (int)new_x;
+	map_y = (int)new_y;
+	
+	if (map_x < 0 || map_y < 0 || map_y >= map_height)
+		return (1);
+		
+	if (!data->map.map[map_y] || map_x >= (int)ft_strlen(data->map.map[map_y]))
+		return (1);
+		
+	if (data->map.map[map_y][map_x] == '1')
+		return (1);
+		
+	return (0);
+}
+*/
